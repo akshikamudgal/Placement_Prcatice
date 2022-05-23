3040fd9aada8445ff25b21fa4d145c026e210294
Akshika Mudgal <66902249+akshikamudgal@users.noreply.github.com>
2022-05-23 21:38:07 +0530

    Create TwoStack.cpp

---
+++ Babbar/TwoStack.cpp
@@ -0,0 +1,49 @@
+class TwoStack{
+  int *arr;
+  int top1;
+  int top2;
+  int size;
+public:
+  
+  //Initialize twoStack
+  TwoStack(int s){
+    this->size=s;
+    top1=-1;
+    top2=s;
+    arr= new int[5];
+  }
+  //Push in Stack 1
+  void push1(int num){
+    //atleast a empty space present
+    
+    if(top2 - top1>1){
+      top1++;
+      arr[top1] =num;
+    }
+  }
+  
+  //pop from stack 1 and return popped element
+  int pop1(){
+        if( top1 >= 0) {
+            int ans = arr[top1];
+            top1--;
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+   // Pop from stack 2 and return popped element.
+    int pop2() {
+		if( top2 < size) {
+            int ans = arr[top2];
+            top2++;
+            return ans;
+        }
+        else
+        {
+            return -1;
+        }
+    }
+};
